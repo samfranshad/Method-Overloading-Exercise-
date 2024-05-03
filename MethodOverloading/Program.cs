8fb998bd93d52a18fe7f96812fb30cd7b7c0258e
Sam Shadwell <sshadwell978@gmail.com>
2024-05-03 15:51:14 -0400

    Forked and cloned, completed exercise 1

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,44 @@
 {
     public class Program
     {
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isCounterfeit)
+        {
+            int sum = numOne + numTwo;
+            if (isCounterfeit == true && sum > 1)
+            {
+                return $"{sum} dollars are counterfeit";
+            }
+            else if(isCounterfeit == true && sum == 1)
+            {
+                return $"{sum} dollar is counterfeit";
+            }
+            else
+            {
+                return "None of the money is counterfeit";
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var sumOne = Add(3, 3);
+            Console.WriteLine(sumOne);
+
+            var sumTwo = Add(4.6m, 6.7m);
+            Console.WriteLine(sumTwo);
+            
+            var totalCounterfeit = Add(0, 1, true);
+            Console.WriteLine(totalCounterfeit);
         }
     }
 }
